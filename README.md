9287806a2fb456157d028d64961cdf514ec937cd
Terra <terra@mcterra.id.au>
2020-12-17 01:32:17 +1100

    initial files
    Signed-off-by: Terra <terra@mcterra.id.au>

---
+++ README.md
@@ -0,0 +1,5 @@
+# OpenGoon API Project
+Open source implementation of the Goonstation server API in PHP (yes I know)
+
+# Usage
+Because it's PHP, usage is braindead simple, as long as you have a DB setup.
---
+++ api/config.php
@@ -0,0 +1,14 @@
+<?php
+// This file is for configuration options for the API. Included by all other files that handle requests.
+
+// Key used to authenticate communications between the BYOND server and the API. Keep this secure.
+$authKey = "auth_key";
+
+// Address used to access the database. SQL file for easy setup included.
+$databaseAddress = "127.0.0.1";
+// DB username
+$databaseUser = "root";
+// DB pass
+$databasePassword = "root";
+
+?>
