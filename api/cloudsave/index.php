feefa6e2571b9033ce34c3e5153038a53a760602
Terra <terra@mcterra.id.au>
2021-01-04 17:51:38 +1100

    the rest of the persistent api
    Signed-off-by: Terra <terra@mcterra.id.au>

--- api/cloudsave/index.php
+++ api/cloudsave/index.php
@@ -27,7 +27,7 @@ if(!$valid) {
 
 /* SAVEFILE METHODS */
 // Store a player's savefile to the DB
-if(key_exists("put", $_GET)) {
+if(key_exists('put', $_GET)) {
 	// Check required params
 	if(!check_params(['ckey', 'name', 'data'], $_GET)) {
 		echo json_error("Missing params");
@@ -47,7 +47,7 @@ if(key_exists("put", $_GET)) {
 	// That's it!
 
 // Delete a player's savefile from DB
-} else if(key_exists("delete", $_GET)) {
+} else if(key_exists('delete', $_GET)) {
 	// Check params
 	if(!check_params(['ckey', 'name'], $_GET)) {
 		echo json_error("Missing params");
@@ -60,7 +60,7 @@ if(key_exists("put", $_GET)) {
 	// Done!
 
 // Get a play's savefil from DB
-} else if(key_exists("get", $_GET)) {
+} else if(key_exists('get', $_GET)) {
 	// Check params
 	if(!check_params(['ckey', 'name'], $_GET)) {
 		echo json_error("Missing params");
@@ -80,9 +80,72 @@ if(key_exists("put", $_GET)) {
 	$return = ['savefile' => $response[0]['data']];
 	echo json_encode($return);
 	return;
+
+/* PERSISTENT DATA METHODS */
+// Put a value in the DB
+} else if(key_exists('dataput', $_GET)) {
+	// Check params
+	if(!check_params(['ckey', 'key', 'value'], $_GET)) {
+		echo json_error("Missing params");
+		return;
+	}
+
+	// Check if a row exists already
+	$result = sql_query("SELECT * FROM `persistent` WHERE `ckey` = ? AND `key` = ?", ['ss', $_GET['ckey'], $_GET['key']]);
+	// Row exists, so we'll overwrite it
+	if($result) {
+		sql_query("UPDATE `persistent` SET `value` = ? WHERE `ckey` = ? AND `key` = ?", ['sss', $_GET['value'], $_GET['ckey'], $_GET['key']]);
+	// Row doesn't exist, so make a new one
+	} else {
+		sql_query("INSERT INTO `persistent` (`ckey`, `key`, `value`) VALUES (?, ?, ?)", ['sss', $_GET['ckey'], $_GET['key'], $_GET['value']]);
+	}
+
+	// Done!
+
+// Get persistent data and list of cloud saves from DB
+} else if(key_exists('list', $_GET)) {
+	// Check params
+	if(!check_params(['ckey'], $_GET)) {
+		echo json_error("Missing params");
+		return;
+	}
+
+	// Now fetch the data from the DB
+	// Let's do savefiles first.
+	$sav_res = sql_query("SELECT * FROM `cloudsaves` WHERE `ckey` = ?", ['s', $_GET['ckey']], true);
+
+	// Now get the persistent data
+	$persist_res = sql_query("SELECT * FROM `persistent` WHERE `ckey` = ?", ['s', $_GET['ckey']], true);
+
+	// Build initial response structure
+	$response = [
+		'saves' => array(),
+		'cdata' => array()
+	];
+
+	// Deal with cloudsave data
+	if($sav_res) {
+		foreach($sav_res as $row) {
+			$response['saves'][$row['name']] = strlen($row['data']);
+		}
+	}
+
+	// Now persistent
+	if($persist_res) {
+		foreach($persist_res as $row) {
+			$response['cdata'][$row['key']] = $row['value'];
+		}
+	}
+
+	// Finally json encode and return.
+	echo json_encode($response);
+	return;
+} else {
+	json_error("Missing params");
+	return;
 }
 
-// Default API return for stuff that doesn't give anything back.
+// Default API return for stuff that doesn't give a response.
 echo JSON_SUCCESS;
 
 ?>
